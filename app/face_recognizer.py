bcdcb9adbaae7cf0079a20ab2e7e9a89a68f6052
lefang <fang.lef@northeastern.edu>
2025-06-24 22:03:22 -0700

    created file structure
    made three files, app for the application and the main logic as well as the ETL pipeline. Input are for user uploads. Output is for the processed videos and the metadata

---
+++ app/face_recognizer.py
@@ -0,0 +1,11 @@
+# import standard libraries
+import cv2
+import os
+import random
+import numpy as np
+
+# import tensorflow functions
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Layer, Conv2D, Dense, MaxPooling2D, Input, Flatten
+import tensorflow as tf
+
