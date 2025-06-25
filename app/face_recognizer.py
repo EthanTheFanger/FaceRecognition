056d69132304aeaf44c7231f1b88e235f3a04cb0
lefang <fang.lef@northeastern.edu>
2025-06-24 22:14:23 -0700

    Update face_recognizer.py

--- app/face_recognizer.py
+++ app/face_recognizer.py
@@ -5,7 +5,7 @@ import random
 import numpy as np
 
 # import tensorflow functions
+import tensorflow as tf
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Layer, Conv2D, Dense, MaxPooling2D, Input, Flatten
-import tensorflow as tf
 
